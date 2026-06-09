2881e4fe81cb4f77cae80c3b45a0fc7f5bceabcc
122045355-pixel <122045355@upq.edu.mx>
2026-06-08 18:58:22 -0600

    Práctica 6

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
+//zona 1 importación de archivos
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {component} from 'react';
+import { StyleSheet, Text, View,Image} from 'react-native';
+import {Saludo} from './components/Saludo';
+import {Saludo2} from './components/Saludo2';
 
+//zona 2 main components
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require('./assets/wave.png')}/>
+      <Text>Hola mundo</Text>
+      <Text>-----------------------------</Text>
+      <Saludo />
+      <Text>-----------------------------</Text>
+      <Saludo2 />
+        
       <StatusBar style="auto" />
     </View>
   );
 }
-
+//zona 3 styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
