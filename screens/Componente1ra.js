1d3ae4432ba51a7a5fd42e862634e30508cb9cab
122045355-pixel <122045355@upq.edu.mx>
2026-06-16 15:36:42 -0600

    Menu

--- screens/Componente1ra.js
+++ screens/Componente1ra.js
@@ -1,14 +1,15 @@
 //zona 1 importación de archivos
 import { StatusBar } from 'expo-status-bar';
 import {component} from 'react';
-import { StyleSheet, Text, View,Image,Button} from 'react-native';
-import MenuScreen from './screens/MenuScreen';
+import { StyleSheet, Text, View,Image} from 'react-native';
 
 
 //zona 2 main components
 export default function App() {
   return (
-    <MenuScreen></MenuScreen>
+    <view>
+        <Text>1ra práctica de componentes nativos</Text>
+    </view>
   );
 }
 //zona 3 styles
