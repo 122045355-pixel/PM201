c045d338d23edfdffcb4abacdf9dd77419a81796
122045355-pixel <122045355@upq.edu.mx>
2026-06-28 08:47:47 -0600

    pagina principal

--- Practica13/repaso1/App.js
+++ Practica13/repaso1/App.js
@@ -13,6 +13,7 @@ export default function App() {
       <ScrollView>
         <Text> Como parte del mundial se les invita a todos los cardenales al concurso de mejores botargas de la upq, los ganadores de este concurso serán premiados dependiendo del lugar en el que hayan teminado: Para el primer lugar tenemos una pc gamer con 16GB de RAM y un procesador i7. para el segundo lugar tenemos una caja con 10 plumas negras, y para el tercer lugar dos bolsas de frijoles isadora.Los ganadores serán escogidos por los profesores de la universidad y se les notificará a los ganadores por correo electrónico. La fecha límite para inscribirse es el 28 de julio del 2026, así que apresúrate y no te quedes fuera de este gran concurso.
         </Text>
+         <Button title="inscribirse" onPress={()=>setScreen(<inscripcion />)}/>
       </ScrollView>
     </SafeAreaView>
     </View>
